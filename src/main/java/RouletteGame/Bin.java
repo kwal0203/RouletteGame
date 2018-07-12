58736c0a04e159ebd3ea654e7b7df5a6c5ce1441
Kane Walter <kwal0203@gmail.com>
2018-07-12 12:16:50 +1000

    Initial Bin commit

---
+++ src/main/java/RouletteGame/Bin.java
@@ -0,0 +1,11 @@
+package RouletteGame;
+
+/**
+ * This class represents a 'bin' of the roulette wheel. A bin is an
+ * area that the ball can come to rest on the wheel after spinning.
+ *
+ * Author: Kane Walter
+ */
+public class Bin {
+
+}
