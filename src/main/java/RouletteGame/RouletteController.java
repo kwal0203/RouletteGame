fe22265b8bfd3aef40b7b4ccf67250972622bd51
Kane Walter <kwal0203@gmail.com>
2018-07-12 16:00:14 +1000

    Changed controller

--- src/main/java/RouletteGame/RouletteController.java
+++ src/main/java/RouletteGame/RouletteController.java
@@ -12,7 +12,7 @@ import java.util.Random;
  */
 @RestController
 public class RouletteController {
-    @RequestMapping("/")
+    @RequestMapping("/simulate")
     public Results simulate(@RequestParam(value = "text",
             defaultValue = "No content provided") String text)
     {
